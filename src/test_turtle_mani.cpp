c5f79ac7991bc9f24b447cf9cb309e5a5e56b5d1
dpfla <49601973+dpfla@users.noreply.github.com>
2020-07-30 16:36:25 +0900

    demo_grip

--- src/test_turtle_mani.cpp
+++ src/test_turtle_mani.cpp
@@ -1,4 +1,4 @@
-#include "needs.hpp"
+#include "test_turtle_mani.hpp"
 
 /*
 bool setJointSpacePath(std::vector<double> joint_angle, double path_time);
@@ -41,11 +41,12 @@ bool OpenMani::setJointSpacePath(std::vector<double> joint_angle, double path_ti
 {
 	ros::AsyncSpinner spinner(1); 
 	spinner.start();
+
 	// Next get the current set of joint values for the group.
 	//JointModelGroup - moveit_fun ,Get a joint group from this model (by name) 
 	//getCurrentState() - Get the current state of the robot. move_Group.cpp 2091
-	const robot_state::JointModelGroup* joint_model_group =
-	move_group_->getCurrentState()->getJointModelGroup("arm");
+
+	const robot_state::JointModelGroup* joint_model_group = move_group_->getCurrentState()->getJointModelGroup("arm");
 	  
 	moveit::core::RobotStatePtr current_state = move_group_->getCurrentState();
 
@@ -77,38 +78,67 @@ bool OpenMani::setJointSpacePath(std::vector<double> joint_angle, double path_ti
 	return true;
 }
 
+bool OpenMani::setToolControl(std::vector<double> joint_angle)
+{
+	ros::AsyncSpinner spinner(1); 
+	spinner.start();
+
+	// Next get the current set of joint values for the group.
+	const robot_state::JointModelGroup* joint_model_group =
+	move_group2_->getCurrentState()->getJointModelGroup("gripper");
+	  
+	moveit::core::RobotStatePtr current_state = move_group2_->getCurrentState();
+
+	std::vector<double> joint_group_positions;
+	current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
+
+	// Now, let's modify one of the joints, plan to the new joint space goal and visualize the plan.
+	joint_group_positions[0] = joint_angle.at(0);  // radians
+	move_group2_->setJointValueTarget(joint_group_positions);
+
+	moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+	bool success = (move_group2_->plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+	if (success == false)
+	return false;
+
+	move_group2_->move();
+
+	spinner.stop();
+	return true;  
+}
+
 
 void OpenMani::updateRobotState()
 {
-	 ros::AsyncSpinner spinner(1); 
-	 spinner.start();
+	ros::AsyncSpinner spinner(1); 
+	spinner.start();
 
-	  std::vector<double> jointValues = move_group_->getCurrentJointValues();
-	  std::vector<double> jointValues2 = move_group2_->getCurrentJointValues();
-	  std::vector<double> temp_angle;
-	  temp_angle.push_back(jointValues.at(0));
-	  temp_angle.push_back(jointValues.at(1));
-	  temp_angle.push_back(jointValues.at(2));
-	  temp_angle.push_back(jointValues.at(3));
-	  temp_angle.push_back(jointValues2.at(0));
+	std::vector<double> jointValues = move_group_->getCurrentJointValues();
+	std::vector<double> jointValues2 = move_group2_->getCurrentJointValues();
+	std::vector<double> temp_angle;
+	temp_angle.push_back(jointValues.at(0));
+	temp_angle.push_back(jointValues.at(1));
+	temp_angle.push_back(jointValues.at(2));
+	temp_angle.push_back(jointValues.at(3));
+	temp_angle.push_back(jointValues2.at(0));
 
-	  geometry_msgs::Pose current_pose = move_group_->getCurrentPose().pose;  
-	  std::vector<double> temp_position;
-	  temp_position.push_back(current_pose.position.x);
-	  temp_position.push_back(current_pose.position.y);
-	  temp_position.push_back(current_pose.position.z);
+	geometry_msgs::Pose current_pose = move_group_->getCurrentPose().pose;  
+	std::vector<double> temp_position;
+	temp_position.push_back(current_pose.position.x);
+	temp_position.push_back(current_pose.position.y);
+	temp_position.push_back(current_pose.position.z);
 
-	  printf("Present Joint Angle J1: %.3lf J2: %.3lf J3: %.3lf J4: %.3lf\n  / Gripper Angle: %.3lf",
-         	temp_angle.at(0),
-         	temp_angle.at(1),
-         	temp_angle.at(2),
-         	temp_angle.at(3), 
-         	temp_angle.at(4));
+	printf("Present Joint J1: %.2lf J2: %.2lf J3: %.2lf J4: %.2lf\n  / Gripper: %.2lf",
+		temp_angle.at(0),
+		temp_angle.at(1),
+		temp_angle.at(2),
+		temp_angle.at(3), 
+		temp_angle.at(4));
 
-      printf("Present Kinematics Position X: %.3lf Y: %.3lf Z: %.3lf\n",
-             temp_position.at(0),
-             temp_position.at(1),
-             temp_position.at(2));
+	printf("Present Kinematics Position X: %.2lf Y: %.2lf Z: %.2lf\n",
+		temp_position.at(0),
+		temp_position.at(1),
+		temp_position.at(2));
 }
 
 void OpenMani::demoSequence()
@@ -121,19 +151,36 @@ void OpenMani::demoSequence()
 	switch(count){
 	case 0: // home pose
 		joint_angle.push_back( 0.00);
-		joint_angle.push_back(-1.05);
-		joint_angle.push_back( 0.35);
-		joint_angle.push_back( 0.70);
-		setJointSpacePath(joint_angle, 1.5);
+		joint_angle.push_back( 1.41);
+		joint_angle.push_back( -0.76);
+		joint_angle.push_back( -0.60);
+		setJointSpacePath(joint_angle, 2.0);
 		count ++;
+		ROS_INFO("case 0");
 		break;
-	case 1: // initial pose
+		
+	case 1:
+		joint_angle.push_back(-0.01);
+		setToolControl(joint_angle);
+		count++;
+		ROS_INFO("case 1");
+		break;
+
+	case 2: // initial pose
 		joint_angle.push_back( 0.00);
-		joint_angle.push_back( 0.00);
-		joint_angle.push_back( 0.00);
-		joint_angle.push_back( 0.00);
-		setJointSpacePath(joint_angle, 1.0);
+		joint_angle.push_back( 0.50);
+		joint_angle.push_back( 0.17);
+		joint_angle.push_back( -0.69);
+		setJointSpacePath(joint_angle, 2.0);
 		count ++;
+		ROS_INFO("case 2");
+		break;
+
+	case 3:
+		joint_angle.push_back(0.01);
+		setToolControl(joint_angle);
+		count=0;
+		ROS_INFO("case 3");
 		break;
 	} // end of switch-case
 
@@ -161,8 +208,9 @@ int main(int argc, char **argv){
 	//ros::start(); // explicitly needed since our nodehandle is going out of scope.
 	ros::NodeHandle nh("");
 	
-	ros::Timer publish_timer = nh.createTimer(ros::Duration(0.100)/*100ms*/, &OpenMani::publishCallback, &OpenMani);
-
+	ROS_INFO("11");
+	ros::Timer publish_timer = nh.createTimer(ros::Duration(4), &OpenMani::publishCallback, &OpenMani);
+	ROS_INFO("11");
 	//ros::start();
 	while (ros::ok())
 	{
